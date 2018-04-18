0ae1ee01cbae9bf99135b318b9a450c44668cba1
zzangman <36238128+zzangman@users.noreply.github.com>
2018-04-18 09:57:28 +0900

    fghhgdfgg

--- src/test01/Test01.java
+++ src/test01/Test01.java
@@ -1,5 +1,10 @@
 package test01;
 
 public class Test01 {
-
+	public static void main(String[] args) {
+		System.out.println("h1");
+		System.out.println("h2");
+		System.out.println("h3");
+		System.out.println("h3");
+	}
 }
